9510a19666a1eded62a2b36258df713c8c9d2c09
fritz-max <fritz@beyondwork.ai>
2025-10-19 15:37:01 +0200

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: mode === 'production' ? '/blops-memory-hunt/' : '/',
   plugins: [
     react(),
     mode === 'development' &&
