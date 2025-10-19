ff10ee9432557e1293a877dd83d30b854ff97769
fritz-max <fritz@beyondwork.ai>
2025-10-19 16:15:40 +0200

    Revert "gh pages"
    This reverts commit 9510a19666a1eded62a2b36258df713c8c9d2c09.

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: mode === 'production' ? '/blops-memory-hunt/' : '/',
   plugins: [
     react(),
     mode === 'development' &&
